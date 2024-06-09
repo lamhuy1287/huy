a842f028b560b2bba73f707cbea642ace3e482d1
Huy <superrabit266@gmail.com>
2024-06-09 09:38:49 +0700

    huy 1

--- customer/home/cardCustomer/showCart.php
+++ customer/home/cardCustomer/showCart.php
@@ -36,12 +36,12 @@ else{
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    
+
     <style>
         * {
             box-sizing: border-box;
         }
-        
+
 
         .header {
             height: 130px;
@@ -228,22 +228,24 @@ else{
             background-color: white;
             color: orange;
         }
-        table{
-          width: 100%;
+
+        table {
+            width: 100%;
         }
-        
+
 
         table,
         th,
         td {
-          text-align:center;
+            text-align: center;
             border: 1px solid black;
             border-collapse: collapse;
-            
+
         }
-        td img{
-          margin-top:5px;
-          margin-bottom:5px;
+
+        td img {
+            margin-top: 5px;
+            margin-bottom: 5px;
         }
 
         .End {
@@ -308,7 +310,7 @@ else{
         </div>
         <div class="header_2">
             <div class="b1">
-            <img id="home_1" style="justify-content: center;" height="80px" width="80px" src="../logo.png" alt="">
+                <img id="home_1" style="justify-content: center;" height="80px" width="80px" src="../logo.png" alt="">
                 <script>
                     document.getElementById("home_1").onclick = function () {
                         location.href = "../home.php";
@@ -361,8 +363,8 @@ else{
 if (empty($max)) {
     // echo "Giỏ hàng hiện chưa có gì??? Bạn hãy đi lựa chọn sản phẩm phù hợp nào";
     ?>
-    <h1>Mau đi mua hàng đi giỏ hàng méo có gì</h1>
-    <?php
+                <h1>Mau đi mua hàng đi giỏ hàng méo có gì</h1>
+                <?php
     exit;   
 }
                 ?>
@@ -401,16 +403,16 @@ for ($i = 0; $i < $max; $i++) {
                         <?php echo "<p class='price'>".$row['price']."$</p>";  ?>
                     </td>
                     <td>
-                        <a class="fa-solid fa-minus" 
-                        onclick="minus()"    href='processAddToCard.php?status=minus&id=<?php echo $array_keys[$i]; ?>'></a>
-                       
+                        <a class="fa-solid fa-minus" onclick="minus()"
+                            href='processAddToCard.php?status=minus&id=<?php echo $array_keys[$i]; ?>'></a>
+
                         <?php
                         echo "<span id='minus'>";
                             echo $_SESSION['cart'][$array_keys[$i]];
                         echo "</span>";
                         ?>
                         <a class="fa-solid fa-plus"
-                             href='processAddToCard.php?status=plus&id=<?php echo $array_keys[$i]; ?>'></a>
+                            href='processAddToCard.php?status=plus&id=<?php echo $array_keys[$i]; ?>'></a>
                     </td>
                     <td>
                         <?php echo "<p class='price'>". $row['price'] * $_SESSION['cart'][$array_keys[$i]]."$</p>"; ?>
@@ -433,16 +435,22 @@ for ($i = 0; $i < $max; $i++) {
         </div>
         <div class="right">
             <div class="right_1">
-          
+
                 <h3>Total order value :</h3>
                 <br>
                 <h4>
-               <?php 
+                    <?php 
                echo " $total $  ";
+               echo "<h1>&#129313</h1>";
                ?>
                 </h4>
                 <hr>
-                <button type="submit" class="orange-button mb-4">Check out</button>
+                <button type="submit" id="checkout" class="orange-button mb-4">Check out</button>
+                <script>
+                    document.getElementById("checkout").onclick = function () {
+                        location.href = "checkout.php";
+                    };
+                    </script>
             </div>
         </div>
     </main>
@@ -459,49 +467,51 @@ for ($i = 0; $i < $max; $i++) {
     </div>
     <br>
     <script>
-    
-    function minus(){
-        // alert("123");
-        let number = document.getElementById('minus').innerHTML;
-        let quantity = parseInt(number);
-        // alert(quantity);
-        if(quantity === 1){
-            confirm("Bạn chắc chắn muốn xóa sản phẩm này???");
+
+        function minus() {
+            // alert("123");
+            let number = document.getElementById('minus').innerHTML;
+            let quantity = parseInt(number);
+            // alert(quantity);
+            if (quantity === 1) {
+                confirm("Bạn chắc chắn muốn xóa sản phẩm này???");
+            }
+
         }
+    </script>
 
-    }
-</script>
-</body>
-<div class="footer">
-    <hr style="border:1px solid black;">
-    <br>
-    <div class="container" style="background-color:white;row row-cols-3">
-        <div class="row w-100">
-            <div class="col">
+    <div class="footer">
+        <hr style="border:1px solid black;">
+        <br>
+        <div class="container" style="background-color:white;row row-cols-3">
+            <div class="row w-100">
+                <div class="col">
 
-                <h3>Liên hệ</h3>
-                <p>Địa chỉ:Phú Diễn , Bắc Từ Liêm ,Hà Nội</p>
-                <p>Email: lamhuy26@gmail.com</p>
-                <p>Điện thoại: 0377006359</p>
-            </div>
-            <div class="col">
-                <h3>Liên kết</h3>
-                <ul>
-                    <li><a href="home.php">Trang chủ</a></li>
-                    <li><a href="new.php">Sản phẩm</a></li>
-                    <li><a href="https://www.messenger.com/e2ee/t/6948976355124079">Liên hệ hỗ trợ</a></li>
-                    <!-- Thêm các liên kết khác -->
-                </ul>
-            </div>
-            <div class="col">
-                <h3>Bản đồ</h3>
-                <div>
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14894.008647910136!2d105.75368688691543!3d21.052596739639352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454dc9b34f767%3A0xd6b847b3f4d5a4a0!2zUGjDuiBEaeG7hW4sIELhuq9jIFThu6sgTGnDqm0sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1711990425983!5m2!1svi!2s"
-                        width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <h3>Liên hệ</h3>
+                    <p>Địa chỉ:Phú Diễn , Bắc Từ Liêm ,Hà Nội</p>
+                    <p>Email: lamhuy26@gmail.com</p>
+                    <p>Điện thoại: 0377006359</p>
+                </div>
+                <div class="col">
+                    <h3>Liên kết</h3>
+                    <ul>
+                        <li><a href="home.php">Trang chủ</a></li>
+                        <li><a href="new.php">Sản phẩm</a></li>
+                        <li><a href="https://www.messenger.com/e2ee/t/6948976355124079">Liên hệ hỗ trợ</a></li>
+                        <!-- Thêm các liên kết khác -->
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>Bản đồ</h3>
+                    <div>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14894.008647910136!2d105.75368688691543!3d21.052596739639352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454dc9b34f767%3A0xd6b847b3f4d5a4a0!2zUGjDuiBEaeG7hW4sIELhuq9jIFThu6sgTGnDqm0sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1711990425983!5m2!1svi!2s"
+                            width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
+</body>
 
 </html>
